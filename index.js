56ad914c2e0f38fa842ef72ab3d5d293a46b814b
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 20:17:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var kittens = ["Milo", "Otis", "Garfield"]
+var kitty = "Ralph"
+var kit = "Snow"
+function destructivelyAppendKitten(kitty) {
+  return kittens.push(kitty);
+}
+function destructivelyPrependKitten(kitty) {
+  return kittens.unshift(kitty);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+function appendKitten(kit) {
+  kittens = [...kittens, kit];
+  return kittens
+}
